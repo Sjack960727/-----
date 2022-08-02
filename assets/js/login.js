392feef7786f4111d6b549be2b75907010a02574
sjk <649937897@qq.com>
2022-08-02 15:43:26 +0800

    登录功能完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -12,7 +12,8 @@ $(function () {
 });
 
 const form = layui.form;
-const baseUrl = "http://www.liulongbin.top:3007";
+const layer = layui.layer;
+// const baseUrl = "http://www.liulongbin.top:3007";
 form.verify({
   // 自定义一个叫 pwd 的校验规则
   pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
@@ -31,10 +32,30 @@ $("#form_reg").on("submit", function (e) {
   const data = $(this).serialize();
   $.ajax({
     type: "POST",
-    url: baseUrl + "/api/reguser",
+    url:"/api/reguser",
     data,
     success: (res) => {
-      console.log(res);
+        if (res.status !== 0) return layer.msg(res.message);
+        layer.msg("注册成功！");
+        // 注册成功后跳转到登录界面
+        $("#link_login").click();
     },
   });
 });
+$('#form_login').on('submit', function (e) {
+    e.preventDefault()
+    data = $(this).serialize();
+    $.ajax({
+        type: "POST",
+        url:"/api/login",
+        data,
+        success: (res) => {
+            if (res.status !== 0) return layer.msg(res.message);
+            layer.msg("登录成功！");
+            // 将登录成功得到的 token 字符串，保存到 localStorage 中
+            localStorage.setItem("token", res.token);
+            // 跳转到主页
+            location.href = "/index.html";
+        }
+    })
+})
